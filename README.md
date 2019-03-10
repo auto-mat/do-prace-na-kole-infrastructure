524b07d542f2732e3e7a386cd1da8ff2dcd1a1d6
Timothy Hobbs <timothy@hobbs.cz>
2019-03-10 16:41:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Do Práce na Kole - infrastruktura
+----------------------------------------
+
+The main repository for the "Do práce na Kole" bike to work competition can be found at [https://github.com/auto-mat/do-prace-na-kole](https://github.com/auto-mat/do-prace-na-kole). These scripts are downloaded by EC2 instances for provisioning and managing docker containers. They are in a separate git repository because they follow a separate release cycle and are downloaded directly by the EC2 instances.
---
+++ aws-startup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
+POSTFIX=$(cat postfix)
+git clone https://git-codecommit.eu-west-1.amazonaws.com/v1/repos/do-prace-na-kole-secrets$POSTFIX secrets
+ln -s secrets/docker.env docker.env
+POSTFIX=$(cat postfix)
+if [ "${POSTFIX}" == "-test" ]; then
+    BRANCH="devel"
+elif  [ "${POSTFIX}" == "-prod" ]; then
+    BRANCH="master"
+fi
+curl https://raw.githubusercontent.com/auto-mat/do-prace-na-kole-infrastructure/$BRANCH/restart_docker_containers > restart_docker_containers
+chmod +x restart_docker_containers
+sh restart_docker_containers
